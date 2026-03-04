e2b666977acb9819dd85bb64729169b540121d32
ursaljam <maykamanatad@gmail.com>
2026-03-04 17:52:21 +0800

    Add login functionality with form submission

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'config.php';
+include_once 'controllers/AuthController.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $authController = new AuthController();
+    $message = $authController->loginUser($_POST['email'], $_POST['password']);
+    echo $message;
+}
+?>
+
+<form method="POST" action="login.php">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
